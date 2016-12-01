453a614355a7fafa4a703c4bf089b632c0be09e3
Chris <3liteHax0r@MCCLOUDTAB>
2016-11-30 22:39:29 -0600

    Address book in progress..

---
+++ AddressBook/AddressBook/Contacts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Contacts
+    {
+        public List<object> Person { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string HomePhone { get; set; }
+        public string CellPhone { get; set; }
+        public string OfficePhone { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+
+        public  Contacts(string firstName, string lastName, string officeNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            OfficePhone = officeNumber;
+        }
+
+
+        //public AdvancedNewContact(string firstName, string lastName, string cellPhone, string officePhone, string email)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
+        //    CellPhone = cellPhone;
+        //    OfficePhone = officePhone;
+        //    Email = email;
+        //}
+
+
+
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            List<Contacts> contacts = new List<Contacts>();
+            var person1 = new Contacts("Jose","Cuervo","8395369871");
+            var person2 = new Contacts("Jim", "Beam","9573548678");
+            contacts.Add(person1);
+            contacts.Add(person2);
+
+            Console.WriteLine("~~~~~~~ Address Book ~~~~~~~");
+            Console.WriteLine("");
+            foreach (Contacts c in contacts)
+            {
+                Console.WriteLine(index++ + ")  Name: " + c.FirstName + " " + c.LastName + "     Office number: "+ c.OfficePhone);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
